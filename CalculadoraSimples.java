891bf6326912f7f493b1f6b995adf59f382b14dc
Carlos Ivan <carlos.ivan1997@outlook.com>
2025-11-24 01:01:04 -0300

    Esqueci de subir o progesso no código

--- CalculadoraSimples.java
+++ CalculadoraSimples.java
@@ -25,5 +25,38 @@ public class CalculadoraSimples {
 
         double resultado = 0;
         boolean operacaoValida = true;
+
+        // Calculo baseado na operação
+        switch (operacao) {
+            case '+':
+                resultado = num1 + num2;
+                break;
+            case '-':
+                resultado = num1 - num2;
+                break;
+            case '*':
+                resultado = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    resultado = num1 / num2;
+                } else {
+                    System.out.println("Erro: Divisão por zero!");
+                    operacaoValida = false; 
+                }
+                break;
+            default:
+                System.out.println("Operação Inválida!");
+                operacaoValida = false;
+                }
+
+        // Exibe o resultado se a operação foi válida
+        if (operacaoValida) {
+            System.out.println("Resultado: " + num1+" " + operacao + " " + num2 +
+    " = " + resultado);
+            }
+
+            scanner.close();
+        }
     }
 }
