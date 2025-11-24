0e635ad51aaea285e47575272370ed1ae2d42615
Carlos Ivan <carlos.ivan1997@outlook.com>
2025-11-24 01:21:33 -0300

    Criando calculado com um código um pouco diferente

---
+++ calculadora2.java
@@ -0,0 +1,5 @@
+// Estava pesquisando e vi que posso fazer de formas diferentes a caluldadora
+// Vou criar esse novo arquivo para testa de outra forma
+// Este código eu achei na internet e vou testa-lo aqui
+// Lembrando que usei IA também para me ajudar a entender o código
+// Deepseek
